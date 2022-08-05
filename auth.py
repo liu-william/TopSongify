1c1356bef0b37d9de94b295ca9b16431c4507142
William Liu <william.liu4518@gmail.com>
2022-08-04 18:26:46 -0600

    feat: added pkce

---
+++ auth.py
@@ -0,0 +1,111 @@
+import string
+import urllib.parse
+import requests
+import base64
+import sys
+import random
+import pkce
+
+REDIRECT_URI = "http://localhost/topsongify/callback/" # NEED TO CHANGE THIS
+CHARACTERS = string.ascii_letters + string.digits
+QUERY = "https://accounts.spotify.com/api/token"
+
+def retrieve_info() -> str: 
+    """
+        Retrieve user info.
+        Returns:
+            client_creds_b64 (str): Client Credentials in byte 64 format.
+            auth_url (str): Authentication URL
+    """
+    ######## REPLACE THESE 2 WITH OWN INFO... ########
+    client_id = input("Enter Client ID: ").strip()
+    client_secret = input("Enter Client Secret: ").strip()
+    ##################################################
+
+    scope = "user-top-read playlist-modify-public playlist-modify-private"
+    client_creds_b64 = f"{client_id}:{client_secret}"
+    client_creds_b64 = base64.b64encode(client_creds_b64.encode()).decode()
+    state = "".join(random.choice(CHARACTERS) for i in range(16))
+    code_verifier = pkce.generate_code_verifier(length=random.choice([i for i in range(43, 129)]))
+    code_challenge = pkce.get_code_challenge(code_verifier)
+
+    auth_url = "https://accounts.spotify.com/authorize"
+    auth_url += "?client_id=" + urllib.parse.quote(client_id)    # URL encode string
+    auth_url += "&response_type=code"
+    auth_url += "&redirect_uri=" + urllib.parse.quote(REDIRECT_URI)    
+    auth_url += "&scope=" + urllib.parse.quote(scope)
+    auth_url += "&state=" + urllib.parse.quote(state)
+    auth_url += "&show_dialog=true"    # To approve app each time
+    auth_url += "&code_challenge_method=S256"
+    auth_url += "&code_challenge=" + urllib.parse.quote(code_challenge)
+
+    print("\nPlease authenticate using this URL: " + auth_url + "\n")
+
+    return client_creds_b64, state, client_id, code_verifier
+
+def authenticate_user(client_creds_b64: str, state: str, client_id: str, code_verifier: str) -> str:
+    """
+        Authenticate the user.
+
+        Args:
+            client_creds_b64 (str): Client Credentials in byte 64 format.
+
+        Returns:
+            (str): User's session access token.
+    """
+
+    redirected_url = input("Please paste the full redirected URL: ")
+
+    # Extract access code
+
+    code = redirected_url[len(REDIRECT_URI + "?code="):-len("&state=" + state)]
+
+    # Request Access token
+    request_data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": REDIRECT_URI,
+        "client_id": client_id,
+        "code_verifier": urllib.parse.quote(code_verifier)
+    }
+
+    request_header = {
+        "Authorization": "Basic " + client_creds_b64, 
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.post(
+        url=QUERY, 
+        data=request_data, 
+        headers=request_header
+    )
+
+    response_json = response.json()
+    
+    # Retrieve access token
+    try:
+        return response_json["access_token"]
+    except KeyError as e:
+        print("\nWrong redirected URL")
+        return
+    
+
+def get_access_token():
+    """
+        Gives users 2 tries to get access token.
+        Returns:
+            access_token (str): User's session access token.
+    """
+    client_creds_b64, state, client_id, code_verifier = retrieve_info()
+    access_token = None
+    tries = 0
+    # Re-authenticate    
+    while not access_token and tries < 2:
+        access_token = authenticate_user(client_creds_b64, state, client_id, code_verifier)
+        tries += 1
+    
+    if access_token:
+        return access_token 
+    
+    print("Failed to authenticate after 2 tries. Program terminating")
+    sys.exit()
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import requests
-from secrets import client_id, client_secret, REDIRECT_URI, access_token, user_id
+from auth import get_access_token
 
-CLIENT_ID = client_id
-CLIENT_SECRET = client_secret
-REDIRECT_URI = REDIRECT_URI
-USER_ID = user_id
+def main():
+    make_playlist = True
+    newPlaylist = CreatePlaylist()
+    user_id = newPlaylist.get_user_id()
+
+    while make_playlist:
+        newPlaylist.get_top_songs()
+        playlist_id = newPlaylist.create_playlist(user_id)
+        newPlaylist.add_to_playlist(playlist_id)
+        make_playlist = True if input("Make another playlist? Y/N: ").strip().upper() == "Y" else False
 
 class CreatePlaylist:
     def __init__(self):
-        self.access_token = access_token
+        """
+            Initialize CreatePlaylist.
+        """
+
+        self.access_token = get_access_token()
         self.tracks = ""
         self.time = 0
         self.time_range = {
@@ -18,12 +28,42 @@ class CreatePlaylist:
         }
         self.limit = 0
 
+
+    def get_user_id(self) -> str:
+        """
+            Get's Spotify user ID.
+
+            Returns:
+                (str): Spotify user's ID.
+        """
+
+        query = "https://api.spotify.com/v1/me"
+
+        request_headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer {}".format(self.access_token)
+        }
+
+        response = requests.get(query, headers=request_headers)
+
+        response_json = response.json()
+
+        return response_json["id"]
+
     def get_top_songs(self):
+        """
+            Gets user's top songs.
+        """
+
+        self.tracks = ""
+
         correct_time = False
         correct_limit = False
 
         while not correct_time: 
-            self.time = int(input(" 1. 4 weeks \n 2. 6 weeks \n 3. All Time \nTime Range? "))
+            print("\nSelect from the following time ranges.")
+            self.time = int(input(" 1. 4 weeks \n 2. 6 weeks \n 3. All Time \n\nTime Range? "))
             correct_time = True if (self.time in self.time_range) else False
             if not correct_time:
                 print("Invalid answer\n")            
@@ -61,11 +101,17 @@ class CreatePlaylist:
    
         self.tracks = self.tracks[:-1]
 
-    def create_playlist(self):
-        # today = date.today()
-        # todayFormatted = today.strftime("%dd/%mm/%Y")
+    def create_playlist(self, user_id: str) -> str:
+        """
+            Creates empty playlist.
+
+            Args: 
+                user_id (str): Spotify user ID.
+            Returns: 
+                (str): New playlist ID.
+        """
         
-        query = "https://api.spotify.com/v1/users/{}/playlists".format(USER_ID)
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(user_id)
 
         request_data = {
             "name": "On Repeat {}".format(" ".join(self.time_range[self.time].split("_")).title()),
@@ -88,26 +134,34 @@ class CreatePlaylist:
 
         return response_json["id"]    # Created playlist ID
 
-    def add_to_playlist(self):
-        
-        self.new_playlist_id = self.create_playlist()
+    def add_to_playlist(self, playlist_id: str):
+        """
+            Populates top songs to playlist.
+            
+            Args: 
+                playlist_id (str): New playlist ID
+        """
 
-        query = "https://api.spotify.com/v1/playlists/{}/tracks?uris={}".format(self.new_playlist_id, self.tracks)
+        query = "https://api.spotify.com/v1/playlists/{}/tracks?uris={}".format(playlist_id, self.tracks)
 
         request_headers = {
             "Accept": "application/json", 
             "Content-Type": "aaplication/json",
             "Authorization": "Bearer {}".format(self.access_token)
         }
+
         response = requests.post(
             query,
             headers=request_headers
         )
 
-def main():
-    a = CreatePlaylist()
-    a.get_top_songs()
-    a.add_to_playlist()
+        response_json = response.json()
+
+        try: 
+            response_json["snapshot_id"]
+            print("Playlist created successfully!\n")
+        except Exception:
+            print("Error creating playlist, try again.")
 
 if __name__ == "__main__":
     main()
